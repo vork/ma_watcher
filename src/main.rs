1a4ac1ea33783c0ffd0fba111a735ce786f62031
Mark Boss <boss@ficus.Informatik.Uni-Tuebingen.De>
2017-08-18 14:42:36 +0200

    Initial commit with project structure

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate tue_raw;
+
+use tue_raw::raw_test;
+
+pub fn main() {
+    raw_test();
+}
---
+++ tue-raw/src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
+
+pub fn raw_test() {
+    println!("Raw works");
+}
